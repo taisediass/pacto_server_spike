332572d68b014cb1865e21a2e2dabff01b5f5f57
taisediass <tdsilva@thoughtworks.com>
2013-09-27 14:54:49 -0300

    +mob adding config for strict matching

--- pactoserver.rb
+++ pactoserver.rb
@@ -2,8 +2,19 @@ require 'goliath'
 require 'httparty'
 
 class PactoServer < Goliath::API
+
+	def options_parser(opts, options)
+		options[:strict] = false
+		opts.on('-x', '--strict', 'Should do strict matching') { |val| options[:strict] = true }
+	end
+
 	def response (env)	
-	resp = HTTParty.get('http://www.mydomain.com:9001/c1', headers: {'Accept' => 'application/json'})
+	# env[Goliath::Request::REQUEST_METHOD]
+	# 	
+	path = env[Goliath::Request::REQUEST_PATH]
+	headers = env['client-headers']
+	resp = HTTParty.get("http://www.mydomain.com:9001#{path}", headers: headers)
+	#resp = HTTParty.get('http://www.mydomain.com:9001/c1', headers: {'Accept' => 'application/json'})
 	[200, {}, resp.body]
 	end
 end
